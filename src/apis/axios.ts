bdfc24707087d4afc60d2a4219bb6ded40f3a6f1
hieu-lee <hiuld1009@gmail.com>
2021-05-31 19:14:50 +0700

    .

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
+  headers: {
+    ApiKeyClient: "b60f9e67-6c99-4ddb-96b6-04b4a6ffa48a",
+  },
   baseURL: "https://hieuleeportfolioapi.azurewebsites.net",
 });
 
