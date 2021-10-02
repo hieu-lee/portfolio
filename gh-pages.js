bd2904e688f5f11a70ccfba4b49f3a14cfd4c6ff
hieu-lee <hiuld1009@gmail.com>
2021-10-02 13:56:46 +0200

    .

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+var ghpages = require("gh-pages");
+
+ghpages.publish(
+  "../public", // path to public directory
+  {
+    branch: "gh-pages",
+    repo: "https://github.com/hieu-lee/portfolio.git", // Update to point to your repository
+    user: {
+      name: "Hieu Lee", // update to use your name
+      email: "hiuld1009@gmail.com", // Update to use your email
+    },
+    dotfiles: true,
+  },
+  () => {
+    console.log("Deploy Complete!");
+  }
+);
