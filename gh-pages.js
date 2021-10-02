9669ac342a2059813a758ac947ccab72aa709807
hieu-lee <hiuld1009@gmail.com>
2021-10-02 13:57:32 +0200

    .

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
 var ghpages = require("gh-pages");
 
 ghpages.publish(
-  "../public", // path to public directory
+  "public", // path to public directory
   {
     branch: "gh-pages",
     repo: "https://github.com/hieu-lee/portfolio.git", // Update to point to your repository
